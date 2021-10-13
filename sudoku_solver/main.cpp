9e17c3e126727af40cd36bcca0a68470409254fa
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-10-13 15:38:55 -0400

    sudoku: create eliminateDupes helper
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- sudoku_solver/main.cpp
+++ sudoku_solver/main.cpp
@@ -21,7 +21,6 @@ struct Cell{
     Cell();
     explicit Cell(char c);
     explicit Cell(int i);
-    char toChar() const;
 
     std::unordered_set<int> vals = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 };
@@ -29,6 +28,8 @@ using Board = std::array<Cell, 81>;
 
 // function declarations
 Board makeBoard(std::vector<std::vector<char>> const& input);
+Board eliminateDupes(Board const& board);
+int makeIndex(int row, int col);
 std::ostream& operator<<(std::ostream& os, Cell const& cell);
 std::ostream& operator<<(std::ostream& os, Board const& board);
 
@@ -36,11 +37,15 @@ int main() {
     Board board = makeBoard(INPUT);
 
     std::cout << board << '\n';
+
+    board = eliminateDupes(board);
+    std::cout << board << '\n';
+
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //                          implementation below
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 /** A single cell on the game board.
  *
@@ -66,6 +71,53 @@ Board makeBoard(std::vector<std::vector<char>> const& input)
     return board;
 }
 
+Board eliminateDupes(Board const& data)
+{
+    Board board(data);
+
+    for (int row = 0; row < 9; row ++) {
+        for (int col = 0; col < 9; col++) {
+            int index = makeIndex(row, col);
+            Cell& cell = board[index];
+            if (cell.vals.size() > 1) {
+                // update based on row
+                for (int _col = 0; _col < 9; _col ++) {
+                    Cell& _cell = board[makeIndex(row, _col)];
+
+                    if (col != _col && _cell.vals.size() == 1) {
+                        cell.vals.erase(*_cell.vals.begin());
+                    }
+                }
+
+                // update based on col
+                for (int _row = 0; _row < 9; _row ++) {
+                    Cell& _cell = board[makeIndex(_row, col)];
+
+                    if (row != _row && _cell.vals.size() == 1) {
+                        cell.vals.erase(*_cell.vals.begin());
+                    }
+                }
+
+                // update based on square
+                int first = (row % 3) * 9 + (col % 3) * 3;
+
+                for (int _inc = 0; _inc < 3; _inc++) {
+                    int _index = first + _inc * 9;
+                    for (int _add = 0; _add < 3; _add++) {
+                        _index += _add;
+                        Cell& _cell = board[_index];
+                        if (makeIndex(row, col) != index && _cell.vals.size() == 1) {
+                            cell.vals.erase(*_cell.vals.begin());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return board;
+}
+
 Cell::Cell(){}
 
 Cell::Cell(char c) {
@@ -75,15 +127,28 @@ Cell::Cell(char c) {
 };
 Cell::Cell(int i) : vals({i}){};
 
-char Cell::toChar() const {
-    return vals.size() > 1
-            ? '.'
-            : (char) (*vals.begin() + 48);
-};
-
 std::ostream& operator<<(std::ostream& os, Cell const& cell)
 {
-    os << cell.toChar();
+    if (cell.vals.size() > 1) {
+        os << '(';
+        int i = 0;
+        for (auto const& val : cell.vals) {
+            os << val;
+
+            if (i < cell.vals.size() - 1) {
+                os << ", ";
+            }
+            else {
+                os << ')';
+            }
+
+            i++;
+        }
+    }
+    else {
+        os << (char) (*cell.vals.begin() + 48);
+    }
+
     return os;
 }
 
