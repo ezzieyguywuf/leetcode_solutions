d7b3e14c65ac8dc000cbf23e6320055ca6c30292
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-10-13 15:03:24 -0400

    sudoku: cleanup after last commit
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- sudoku_solver/main.cpp
+++ sudoku_solver/main.cpp
@@ -26,27 +26,16 @@ struct Cell{
     std::unordered_set<int> vals = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 };
 using Board = std::array<Cell, 81>;
-using IndexRow = std::array<std::size_t, 9>;
-using Indices = std::array<IndexRow, 9>;
 
 // function declarations
 Board makeBoard(std::vector<std::vector<char>> const& input);
-constexpr Indices makeIndices(bool makeRows = true);
-constexpr Indices makeSquareIndices();
 std::ostream& operator<<(std::ostream& os, Cell const& cell);
 std::ostream& operator<<(std::ostream& os, Board const& board);
 
-
 int main() {
     Board board = makeBoard(INPUT);
-    /* Indices rows = makeIndices(); */
-    /* Indices cols = makeIndices(false); */
-    /* Indices sqrs = makeSquareIndices(); */
 
     std::cout << board << '\n';
-    /* printTable(rows); */
-    /* printTable(cols); */
-    /* printTable(sqrs); */
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -75,55 +64,6 @@ Board makeBoard(std::vector<std::vector<char>> const& input)
     }
 
     return board;
-}
-
-constexpr Indices makeIndices(bool makeRows) {
-    Indices out;
-
-    for(int i = 0; i < 9; i++) {
-        IndexRow row;
-        for (int j = 0; j < 9; j++) {
-            if (makeRows) {
-                row[j] = i * 9 + j;
-            }
-            else {
-                row[j] = i + j * 9;
-            }
-        }
-
-        out[i] = row;
-    }
-
-    return out;
-}
-
-constexpr Indices makeSquareIndices()
-{
-    Indices out;
-
-    for(int i = 0; i < 9; i++) {
-        IndexRow sqr;
-        for (int j = 0; j < 9; j++) {
-            int index = 3 * (i % 3) + 9 * (j % 3);
-            if (j >= 3 && j < 6) {
-                index += 1;
-            }
-            else if (j >=6) {
-                index += 2;
-            }
-            if (i >= 3 && i < 6) {
-                index += 27;
-            }
-            else if (i >= 6) {
-                index += 54;
-            }
-            sqr[j] = index;
-        }
-
-        out[i] = sqr;
-    }
-
-    return out;
 }
 
 Cell::Cell(){}
